a0043934fcfe7da27125d3c9ead72dba5f226fc8
Gustominox <agstcmps.111@gmail.com>
2022-10-24 15:43:51 +0100

    Small changes in logs

--- projeto/testes/logs.py
+++ projeto/testes/logs.py
@@ -5,16 +5,16 @@ argv = sys.argv[1:]
 
 def logLevel(level):
     levels = {
-        0: logging.DEBUG,
-        1: logging.INFO,
-        2: logging.WARNING,
-        3: logging.ERROR,
-        4: logging.CRITICAL,
+        1: logging.DEBUG,
+        2: logging.INFO,
+        3: logging.WARNING,
+        4: logging.ERROR,
+        5: logging.CRITICAL,
     }
 
     return levels.get(level,0)    
 
-logging.basicConfig(level=0,
+logging.basicConfig(level=logLevel(int(argv[0])),
                     format='%(asctime)s %(levelname)8s \t%(message)s',
                     filename='./tmp/myapp.log',
                     filemode='w',
