17c4302e35b3f8960f89ecb809d31199cd130adb
Gustominox <agstcmps.111@gmail.com>
2022-11-14 15:03:04 +0000

    Small changes to client_tcp

--- projeto/cliente_tcp.py
+++ projeto/cliente_tcp.py
@@ -1,17 +1,18 @@
 import socket
 import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def send_tcp( endereco='127.0.0.1', porta=3333):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect(('127.0.0.1', 3333))
+        s.connect((endereco, porta))
 
+        i = 0
+        while True and i < 10:
+            msg = f"Adoro Redes :) [{i}]"
+            s.sendall(msg.encode('utf-8'))
+            time.sleep(1)
+            i += 1
 
+        s.close()
 
-i = 0
-while True and i < 10:
-    msg = f"Adoro Redes :) [{i}]"
-    s.sendall(msg.encode('utf-8'))
-    time.sleep(1)
-    i += 1
-    
-s.close()
+send_tcp()
