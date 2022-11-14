968b7eceb2c666b750e5afb468200c5a85dbc28b
Gustominox <agstcmps.111@gmail.com>
2022-11-14 13:49:36 +0000

    Introduce debug module

--- projeto/cliente.py
+++ projeto/cliente.py
@@ -1,27 +1,41 @@
 import logging as log
 import socket
 import sys
+from debug_module import *
+
+LOGFILE = '/tmp/client.log'
+
+DEBUG_MODE = True
 
 
-class cliente:
+class Client:
+
+    def __init__(self):
+
+        innit_log(LOGFILE)
+
+        self.soc = socket.socket(socket.AF_INET,       # Familia de aderecos ipv4
+                                 socket.SOCK_DGRAM)    # Connection less (UDP PROTOCOL)
+
+    def send(self, msg, endereco):
+        try:
+            self.soc.sendto(msg.encode('utf-8'), (endereco, 3333))
+            debug("Mensagem enviada")
+
+        except:
+            exception("Impossivel enviar mensagem")
+
+
+def main():
 
     endereco = sys.argv[1]
 
-    log.basicConfig(level=log.DEBUG,
-                    format='%(asctime)s:%(levelname)-8s: %(message)s',
-                    filename='/tmp/cliente.log',
-                    filemode='w',
-                    datefmt='%d-%m-%y %H:%M:%S')
-
-    s = socket.socket(socket.AF_INET,       # Familia de aderecos ipv4
-                      socket.SOCK_DGRAM)    # Connection less (UDP PROTOCOL)
-
-    # hostname = socket.gethostname()
-    # endereco = socket.gethostbyname(hostname)   #'10.0.0.1'
-
     msg = "Adoro Redes XD"
-    try:
-        s.sendto(msg.encode('utf-8'), (endereco, 3333))
-        log.debug("Mensagem enviada")
-    except:
-        log.exception("Impossivel enviar mensagem")
+
+    client = Client()
+    # print(f'{msg}  {endereco}')
+    client.send(msg, endereco)
+
+
+if __name__ == "__main__":
+    main()
