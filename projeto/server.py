a30ec0bc8ec2959686a923281aeaf8fab7cbb4eb
Gustominox <agstcmps.111@gmail.com>
2022-11-14 13:51:05 +0000

    Changes in the way that mode debug and import config is implemented, these are now seperate modules

--- projeto/server.py
+++ projeto/server.py
@@ -1,41 +1,86 @@
+from debug_module import *
+from config import config
 import logging as log
 import socket
+import threading
+import time
+
+LOGFILE = '/tmp/server.log'
 
 
 class Server:
 
     def __init__(self):
 
-        log.basicConfig(level=log.DEBUG,
-                        format='%(asctime)s %(levelname)8s \t%(message)s',
-                        filename='/tmp/server.log',
-                        filemode='w',
-                        datefmt='%d-%m-%y %H:%M:%S')
-
-        self.soc = socket.socket(socket.AF_INET,     # Familia de aderecos ipv4
-                                 socket.SOCK_DGRAM)  # Connection less (UDP PROTOCOL)
-
+        innit_log(LOGFILE)
+        
         self.hostname = socket.gethostname()
         self.endereco = socket.gethostbyname(self.hostname)  # '10.0.0.1'
         self.porta = 3333
 
-    def run(self):
+        self.conf = config()
 
-        self.soc.bind(('', self.porta))
+    def run_udp(self):
 
-        log.debug(f"Estou à escuta no {self.endereco}:{self.porta}")
+        soc = socket.socket(socket.AF_INET,     # Familia de aderecos ipv4
+                            socket.SOCK_DGRAM)  # Connection less (UDP PROTOCOL)
+
+        soc.bind(('', self.porta))
+
+        debug(f"Estou à escuta no {self.endereco}:{self.porta}")
 
         while True:
             msg, add = self.soc.recvfrom(1024)
-            log.debug(f"MESSAGE RECEIVED: \"{msg.decode('utf-8')}\"")
-            log.debug(f"Recebi uma mensagem do cliente {add}")
+            debug(f"MESSAGE RECEIVED: \"{msg.decode('utf-8')}\"")
+            debug(f"Recebi uma mensagem do cliente {add}")
 
         self.soc.close()
+
+    def processamento(self, connection, address):
+        while True:
+            msg = connection.recv(1024)
+
+            if not msg:
+                debug("Conexao Terminada")
+                break
+
+            debug(msg.decode('utf-8'))
+            debug(f"Recebi uma ligação do cliente {address}")
+
+        connection.close()
+
+    def run_tcp(self):
+        s = socket.socket(socket.AF_INET,     # Familia de aderecos ipv4
+                          socket.SOCK_STREAM)  # Connection-Oriented (UCP PROTOCOL)
+
+        s.bind(('', self.porta))
+        s.listen()
+
+        print(f"Estou à escuta no {self.endereco}:{self.porta}")
+
+        while True:
+            connection, address = s.accept()
+
+            threading.Thread(target=self.processamento, args=(
+                connection, address)).start()
+
+        s.close()
+
+   
+
+    def config(self, filename):
+        try:
+            self.conf.read(filename)
+            debug("Leu config com sucesso")
+        except:
+            exception("Erro ao ler config")
 
 
 def main():
     server = Server()
-    server.run()
+    server.config("./etc/sp.conf")
+    debug(server.conf.getlines())
+    server.run_tcp()
 
 
 if __name__ == "__main__":
